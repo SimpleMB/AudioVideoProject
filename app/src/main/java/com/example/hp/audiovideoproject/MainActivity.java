878223b22fcff2aa109ab1a33c6efc562e6af850
SimpleMB <ab.mazur@gmail.com>
2018-12-13 20:03:25 +0100

    Commit no2.
    1. App become very slow so I changed implementation of setOnSeekBarListener to local initialization. Didn't help at all. One listener works fine. After adding that second app slows down.

--- app/src/main/java/com/example/hp/audiovideoproject/MainActivity.java
+++ app/src/main/java/com/example/hp/audiovideoproject/MainActivity.java
@@ -6,17 +6,17 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.SeekBar;
 import android.widget.Toast;
 import android.widget.VideoView;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity {
 
     private VideoView myVideoView;                          // setting new variable of VideoView type
     private Button btnPlay, btnPlayMusic, btnPauseMusic;    //
@@ -39,9 +39,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seekBarVolume = findViewById(R.id.seekBarVolume);
         seekBarMove = findViewById(R.id.seekBarMove);
 
-        btnPlay.setOnClickListener(MainActivity.this);                      // setting onClickListeners to buttons
-        btnPlayMusic.setOnClickListener(MainActivity.this);
-        btnPauseMusic.setOnClickListener(MainActivity.this);
+
+        btnPlay.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View vid) {
+
+                myVideoView.start();        // start playing video
+
+            }
+        });                      // setting onClickListeners to buttons
+
+
+        btnPlayMusic.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View music) {
+
+                myMusic.start();            //start playing music
+
+            }
+        });
+
+
+        btnPauseMusic.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View pause) {
+
+                myVideoView.pause();
+                myMusic.pause();
+
+            }
+        });
 
         mediaControler = new MediaController(MainActivity.this);    // creating new media controller object
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);      // creating (getting) new Audio Service
@@ -51,28 +78,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int currentVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         seekBarVolume.setMax(maximumVol);
         seekBarVolume.setProgress(currentVol);
-        seekBarVolume.setOnSeekBarChangeListener(MainActivity.this);
-//                (new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                if (fromUser) {
-//                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
-//                }
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
+        seekBarVolume.setOnSeekBarChangeListener (new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         Uri uriVid = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.power);   // setting path to video file in res/raw folder
-        // and adding it to new variable of type Uri
+                                                                                                // and adding it to new variable of type Uri
         myVideoView.setVideoURI(uriVid);                    // adding file from path to VideoView
         myVideoView.setMediaController(mediaControler);     // add media controller to VideoView
         mediaControler.setAnchorView(myVideoView);          // anchor media controller to VideoView
@@ -84,56 +110,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Seek Bar for progress of playing
 
-        seekBarMove.setOnSeekBarChangeListener(MainActivity.this);          // thanks to implementing interface SeekBar.OnSeekBarChangeListener
-                                                                            // in MainActivity class we only have to activate setOnSeekBarChangeListener
-        seekBarMove.setMax(myMusic.getDuration());
+//        seekBarMove.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });            // thanks to implementing interface SeekBar.OnSeekBarChangeListener
+//                                                                                // in MainActivity class we only have to activate setOnSeekBarChangeListener
+//        seekBarMove.setMax(myMusic.getDuration());
+//
+//    }
+//
 
-    }
-
-    @Override
-    public void onClick(View btnClicked) {
-
-        switch (btnClicked.getId()) {
-
-            case R.id.btnPlay:
-                myVideoView.start();        // start playing video
-                break;
-            case R.id.btnPlayMusic:
-                myMusic.start();            //start playing music
-                timer = new Timer();
-                timer.scheduleAtFixedRate(new TimerTask() {
-                    @Override
-                    public void run() {
-
-                        seekBarMove.setProgress(myMusic.getCurrentPosition());
-                    }
-                }, 0, 1000);
-                break;
-            case R.id.btnPauseMusic:
-                myVideoView.pause();        //video paused
-                myMusic.pause();            //music paused
-                timer.cancel();             // cancels timer progress thread
-                break;
-        }
-
-    }
-
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (fromUser) {
-              audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
-
-        }
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-
-    }
-}
+//    @Override
+//    public void onClick(View btnClicked) {
+//
+//        switch (btnClicked.getId()) {
+//
+//            case R.id.btnPlay:
+//                myVideoView.start();        // start playing video
+//                break;
+//            case R.id.btnPlayMusic:
+//                myMusic.start();            //start playing music
+////                timer = new Timer();
+////                timer.scheduleAtFixedRate(new TimerTask() {
+////                    @Override
+////                    public void run() {
+////
+////                        seekBarMove.setProgress(myMusic.getCurrentPosition());
+////                    }
+////                }, 0, 1000);
+////                break;
+//            case R.id.btnPauseMusic:
+//                myVideoView.pause();        //video paused
+//                myMusic.pause();            //music paused
+////                timer.cancel();             // cancels timer progress thread
+//                break;
+//        }
+//
+//    }
+//
+//
+//    @Override
+//    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//        if (fromUser) {
+//              audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+//
+//        }
+//    }
+//
+//    @Override
+//    public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//    }
+//
+//    @Override
+//    public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//    }
+}}
